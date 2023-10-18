fa544a9a8ae79e8733e9736123a098358f2295d3
br3tdev <brtdev2@gmail.com>
2023-10-18 10:36:54 -0400

    Sidebar Item

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,11 +1,19 @@
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon } from "lucide-react";
+import {
+  ChevronsLeft,
+  MenuIcon,
+  PlusCircle,
+  Search,
+  Settings,
+} from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import UserItem from "./user-item";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import Item from "./item";
+import { toast } from "sonner";
 
 export interface INavigationProps {}
 
@@ -96,6 +104,18 @@ export default function Navigation(props: INavigationProps) {
     }
   };
 
+  const create = useMutation(api.documents.create);
+
+  const handleCreate = () => {
+    const promise = create({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating document...",
+      success: "Document created!",
+      error: "Failed to create document!",
+    });
+  };
+
   return (
     <>
       <aside
@@ -118,6 +138,9 @@ export default function Navigation(props: INavigationProps) {
         </div>
         <div>
           <UserItem />
+          <Item label="Search" icon={Search} isSearch onClick={() => {}} />
+          <Item label="Settings" icon={Settings} onClick={() => {}} />
+          <Item onClick={handleCreate} label="New Document" icon={PlusCircle} />
         </div>
         <div className="mt4">
           <p>
