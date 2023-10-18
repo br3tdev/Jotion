9d29cd2b9fafee34202c822cbc0e1b0983e761c3
br3tdev <brtdev2@gmail.com>
2023-10-18 06:23:46 -0400

    Sidebar

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,9 @@
+export interface IDocumentsPageProps {}
+
+export default function DocumentsPage(props: IDocumentsPageProps) {
+  return (
+    <div>
+      <h1>This should be a protected page</h1>
+    </div>
+  );
+}
