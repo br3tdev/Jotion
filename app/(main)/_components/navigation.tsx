b9f6d94ea8f18d84d253e64fc0c069d8f932b341
br3tdev <brtdev2@gmail.com>
2023-10-19 07:31:06 -0400

    Settings Modal

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -24,11 +24,13 @@ import {
 } from "@/components/ui/popover";
 import TrashBox from "./trash-box";
 import { useSearch } from "@/hooks/use-search";
+import { useSettings } from "@/hooks/use-settings";
 
 export interface INavigationProps {}
 
 export default function Navigation(props: INavigationProps) {
   const search = useSearch();
+  const settings = useSettings();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
 
@@ -147,7 +149,7 @@ export default function Navigation(props: INavigationProps) {
         <div>
           <UserItem />
           <Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
-          <Item label="Settings" icon={Settings} onClick={() => {}} />
+          <Item label="Settings" icon={Settings} onClick={settings.onOpen} />
           <Item onClick={handleCreate} label="New Page" icon={PlusCircle} />
           <Popover>
             <PopoverTrigger className="w-full mt-4">
